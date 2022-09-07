affde164123e6f5715d87f9dd6bcdbfb65c01565
ShovalShabi <shovalshabi@gmail.com>
2022-09-07 14:23:00 +0300

    Update and rename Asgard Shift Orgenizer/Properties/Resources.Designer.cs to Shift Orgenizer/Properties/Resources.Designer.cs

--- Shift Orgenizer/Properties/Resources.Designer.cs
+++ Shift Orgenizer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Asgard_Shift_Orgenizer.Properties {
+namespace Shift_Orgenizer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Asgard_Shift_Orgenizer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Asgard_Shift_Orgenizer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Shift_Orgenizer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
